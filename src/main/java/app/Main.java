4b06a417cd171107585828ef6259a6f4e97560c6
Guilherme China <echina725@gmail.com>
2024-03-18 19:27:09 -0300

    add test method

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,5 +3,10 @@ package app;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!");
+        Main.test("Test message");
+    }
+
+    public static void test(String message) {
+        System.out.println(message);
     }
 }
