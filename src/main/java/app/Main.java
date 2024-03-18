bd7b503f91ca37c854b2c0ac198304a69e0e5673
Guilherme China <echina725@gmail.com>
2024-03-18 20:25:08 -0300

    add test package

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,9 +1,13 @@
 package app;
+import testPackage.testClass;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!");
         Main.test("Test message");
+
+        testClass test = new testClass("churros", 16);
+        System.out.println(test.getName());
     }
 
     public static void test(String message) {
