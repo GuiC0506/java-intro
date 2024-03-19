ea68aadadfde8b4c62c707988cc1ef047c5de365
Guilherme China <echina725@gmail.com>
2024-03-19 19:05:08 -0300

    add scanner package

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,13 +1,11 @@
 package app;
-import testPackage.testClass;
+import PScanner.FScanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!");
         Main.test("Test message");
-
-        testClass test = new testClass("churros", 16);
-        System.out.println(test.getName());
+        FScanner.Start();
     }
 
     public static void test(String message) {
