72f408692806131f9cdbd03bba157885f093c007
Guilherme China <echina725@gmail.com>
2024-03-19 20:33:04 -0300

    finish string package

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,13 +1,13 @@
 package app;
 import PScanner.FScanner;
-import app.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!");
         Main.test("Test message");
         // FScanner.Start();
-        Arrays.Start();
+        // Arrays.Start();
+        FStrings.Start();
     }
 
     public static void test(String message) {
