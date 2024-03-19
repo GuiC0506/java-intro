08f10cd9521408ff62660936d3a789a1aa199933
Guilherme China <echina725@gmail.com>
2024-03-19 19:20:49 -0300

    add array package

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,11 +1,13 @@
 package app;
 import PScanner.FScanner;
+import app.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!");
         Main.test("Test message");
-        FScanner.Start();
+        // FScanner.Start();
+        Arrays.Start();
     }
 
     public static void test(String message) {
