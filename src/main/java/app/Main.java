7e424d75fc38dd19e27ca045dfd89eeda95c1ba9
Guilherme China <echina725@gmail.com>
2024-03-24 21:00:32 -0300

    add employee class

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -26,24 +26,11 @@ public class Main {
         System.out.println(fear.getLevel());
         
         // Exceptions.Start();
-        Cat cat = new Cat();
-        cat.print("Meow");
-        // since Cat class implements the "IPrintable" interface, it can be passed as a parameter
-        printThing(cat);
-        // lambdas work with parameters typed by an interface
-        // instead of passing an object that contains an implementation of a specific method
-        // defined inside an interface, it can be passed a lambda expression that does that
-        // action.
-        printThing(suffix ->  System.out.println("Meow"));
-
-        IPrintable lambdaPrintable = (suffix) -> System.out.println(suffix);
-        }
-
-        public static void test(String message) {
-            System.out.println(message);
-        }
-
-        public static void printThing(IPrintable thing) {
-            thing.print("Meow");
-        }
+        Lambdas.Start();
     }
+
+    public static void test(String message) {
+        System.out.println(message);
+    }
+
+}
